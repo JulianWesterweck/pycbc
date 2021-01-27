9fad6d94d46a18389132fe8c74491ba5d193892d
Julian Westerweck <julian.westerweck@aei.mpg.de>
2021-01-27 19:39:43 +0000

    WIP: Introduce taper for echo Gaussian noise model.

--- pycbc/inference/models/gaussian_noise.py
+++ pycbc/inference/models/gaussian_noise.py
@@ -25,7 +25,7 @@ from scipy import special
 
 from pycbc import filter as pyfilter
 from pycbc.waveform import (NoWaveformError, FailedWaveformError)
-from pycbc.waveform import generator
+from pycbc.waveform import generator, utils
 from pycbc.types import Array, FrequencySeries
 from pycbc.strain import gates_from_cli
 from pycbc.strain.calibration import Recalibrate
@@ -269,7 +269,8 @@ class BaseGaussianNoise(BaseDataModel):
 
             ws = d.copy()
             ws[kmin:kmax] *= w[kmin:kmax]
-            ws = ws.to_timeseries()
+#            ws = ws.to_timeseries()
+            ws = utils.fd_to_td(ws, left_window=(self._f_lower[det]-5.,self._f_lower[det]))
             ws = ws[int(len(ws)/4):int(3/4*len(ws))]
             self._whitened_data_short[det] = ws.to_frequencyseries()
 
@@ -997,10 +998,10 @@ class GaussianNoiseEcho(BaseGaussianNoise):
         params['tc'] = params['tc'] - params['t_final']/2.
         params['t_final'] = 2 * params['t_final']
         temp_freq = params['f_220']
-        params['f_220'] = 10.
+        params['f_220'] = 8.
         params['amp220'] = params['amp220'] * numpy.exp(512./params['tau_220'])
         # Set width of frequency window around peak in frequency steps
-        freq_window_idx = int(20. / self.override_delta_f)
+#        freq_window_idx = int(20. / self.override_delta_f)
         try:
             wfs = self.waveform_generator.generate(**params)
         except NoWaveformError:
@@ -1015,7 +1016,7 @@ class GaussianNoiseEcho(BaseGaussianNoise):
         for det, h in wfs.items():
             shift_idx = int((temp_freq - params['f_220']) / h.delta_f)
             h_data = numpy.zeros(int(256./h.delta_f+1), dtype=h.dtype)
-            h_data[shift_idx:shift_idx+freq_window_idx] = h.data[:freq_window_idx]
+            h_data[shift_idx:shift_idx+len(h)] = h.data[:len(h)]
             h = FrequencySeries(h_data, delta_f=h.delta_f, epoch=h.epoch)
             # the kmax of the waveforms may be different than internal kmax
             kmax = min(len(h), self._kmax[det])
@@ -1029,7 +1030,8 @@ class GaussianNoiseEcho(BaseGaussianNoise):
                 # whiten the waveform
                 h[slc] *= self._weight[det][slc]
                 # remove beginning and end padding in time domain
-                h = h.to_timeseries()
+#                h = h.to_timeseries()
+                h = utils.fd_to_td(h, left_window=(self._f_lower[det]-5.,self._f_lower[det]))
                 h = h[int(len(h)/4):int(3*len(h)/4)]
                 n_wf = len(h)
                 h = h.to_frequencyseries()
