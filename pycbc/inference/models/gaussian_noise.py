2c308afea7aaaa11731dc20247b7ea6f7ad17a36
Julian Westerweck <julian.westerweck@aei.mpg.de>
2021-02-17 17:04:22 +0000

    Do not restrict whitening to band used for likelihood.

--- pycbc/inference/models/gaussian_noise.py
+++ pycbc/inference/models/gaussian_noise.py
@@ -257,17 +257,11 @@ class BaseGaussianNoise(BaseDataModel):
                 p = psds[det].copy()
             self._psds[det] = p
             # we'll store the weight to apply to the inner product
-            w = Array(numpy.zeros(len(p)))
-            # only set weight in band we will analyze
-            kmin = self._kmin[det]
-            kmax = self._kmax[det]
-            invp = FrequencySeries(numpy.zeros(len(p)), delta_f=p.delta_f)
-            invp[kmin:kmax] = 1./p[kmin:kmax]
-            w[kmin:kmax] = numpy.sqrt(4 * invp.delta_f * invp[kmin:kmax])
+            invp = 1./p
             self._invpsds[det] = invp
-            self._weight[det] = w
+            self._weight[det] = numpy.sqrt(4 * invp.delta_f * invp)
             self._whitened_data[det] = d.copy()
-            self._whitened_data[det][kmin:kmax] *= w[kmin:kmax]
+            self._whitened_data[det] *= self._weight[det]
         # set the lognl and lognorm; we'll get this by just calling lognl
         _ = self.lognl
 
