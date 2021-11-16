d97cc6281d4799e743b5cbc7fb77ebc77c42d2fa
Julian Westerweck <julian.westerweck@aei.mpg.de>
2021-11-16 18:37:40 +0000

    Add required args to sine waveform.

--- pycbc/waveform/sine.py
+++ pycbc/waveform/sine.py
@@ -1,16 +1,36 @@
 import numpy
 from pycbc.types import TimeSeries
+from pycbc.waveform.waveform import get_obj_attrs
 
-def td_sine(template=None, **kwargs):
+required_args = ['freq', 'phi', 'amp', 'inclination', 't_final']
+td_args = {'delta_t': None, 't_final': None, 'taper': False}
+
+def props(obj, required, domain_args, **kwargs):
+    """ Return a dictionary built from the combination of defaults, kwargs,
+    and the attributes of the given object.
+    """
+    # Get the attributes of the template object
+    pr = get_obj_attrs(obj)
+
+    # Get the parameters to generate the waveform
+    # Note that keyword arguments override values in the template object
+    input_params = domain_args.copy()
+    input_params.update(pr)
+    input_params.update(kwargs)
+    # Check if the required arguments are given
+    for arg in required:
+        if arg not in input_params:
+            raise ValueError('Please provide ' + str(arg))
+
+    return input_params
+
+def td_sine(**kwargs):
     """
     Generates a sine waveform in the time domain,
     starting abruptly at tc.
 
     Parameters
     ----------
-    template : object
-        An object that has attached properties. This can be used to substitute
-        for keyword arguments. A common example would be a row in an xml table.
     freq : float
         Frequency of the sine wave.
     phi : float
@@ -51,7 +71,42 @@ def td_sine(template=None, **kwargs):
 
     return wf.real(), wf.imag()
 
+def get_td_sine(template=None, **kwargs):
+    """
+    Generates a sine waveform in the time domain,
+    starting abruptly at tc.
+
+    Parameters
+    ----------
+    template : object
+        An object that has attached properties. This can be used to substitute
+        for keyword arguments. A common example would be a row in an xml table.
+    freq : float
+        Frequency of the sine wave.
+    phi : float
+        Initial phase of the sine wave. phi=0 yields
+        a cosine for the plus- and a sine for the cross-polarisation
+        relative to the reference time.
+    amp : float
+        Amplitude of the sine wave.
+    t_final : float
+        Duration of the sine wave.
+    inclination : float
+        Inclination of the source w.r.t the vector towards the observer.
+    delta_t : float
+        Time step for the waveform.
+
+    Returns
+    hplus : TimeSeries
+        The plus polarisation of the sine waveform.
+    hcross : TimeSeries
+        The cross polarisation of the sine waveform.
+    -------
+    """
+    input_params = props(template, required_args, td_args, **kwargs)
+    return td_sine(input_params)
+
 # Approximant names ###########################################################
 sine_td_approximants = {
-    'TdSine': td_sine
+    'TdSine': get_td_sine
     }
